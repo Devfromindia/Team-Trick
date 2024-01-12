e06282e6f0332710fee77c5a91f0d3d64fdc9291
Dev_In <156421372+Devfromindia@users.noreply.github.com>
2024-01-12 15:42:21 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function Rick()
+{
+
+    window.location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+
+}
+function inv()
+{
+
+    window.location.replace("https://discord.gg/Er9N5Uhc");
+
+}
